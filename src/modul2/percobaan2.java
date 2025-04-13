c0cbdb5fd4223409f7da642b0dfea80117732074
cahyaayaa <chyanna790@gmail.com>
2025-04-13 21:30:19 +0700

    tugas4

--- src/modul2/percobaan2.java
+++ src/modul2/percobaan2.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modul2;
-
+import java.awt.HeadlessException;
 import javax.swing.JOptionPane;
 
 /**
